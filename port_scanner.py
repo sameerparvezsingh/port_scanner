754962d2f842608cc2c33227ed2d1b8c7f0f05ac
Sameer P Singh <sam.parvez116@gmail.com>
2024-01-05 16:31:51 -0500

    initial build

---
+++ port_scanner.py
@@ -0,0 +1,41 @@
+from socket import *
+import sys, time
+from datetime import datetime
+
+host = ''
+max_port = 5
+min_port = 1
+
+def scan_host(host, port, r_code):
+    try:
+        s = socket(AF_INET, SOCK_STREAM) #using OS scoket function to connect to intended target
+
+        connection = s.connect_ex(host, port)
+
+        if connection==0:
+            r_code = connection
+        s.close()
+    except Exception as e:
+        print(f"Error encountered: {e}")
+    
+    return r_code
+
+try:
+    host = input("[*] Enter target host address: *")
+except KeyboardInterrupt:
+    print("[*] Interupt detected. Exiting program.")
+    sys.exit(1)
+
+host_ip = gethostbyname(host)
+print(f"\n[*] Host: {host}, IP:{host_ip}.")
+
+print(f"**Starting scanning.**")
+
+for port in range(min_port, max_port):
+    try:
+        response = scan_host(host, port)
+
+        if response == 0:
+            print(f"Port: {port} status = open")
+    except Exception as e:
+        print(f"The error is: {e}")
